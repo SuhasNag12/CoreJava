02384dc66d6ce9ec20e0fdae835a012429fe5bd5
Suhas Nag <suhas.nag@perficient.com>
2025-07-27 18:46:35 +0530

    Shopping Cart

--- src/App.java
+++ src/App.java
@@ -3,18 +3,17 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to finding Area of a rectangle.");
+        System.out.print("Enter the Item you would like to buy: ");
+        String itemName = scanner.nextLine();
 
-        System.out.print("Enter Lenght of the Rectangle: ");
-        double rectLength = scanner.nextDouble();
+        System.out.print("Enter the Price for each: ");
+        double itemPrice = scanner.nextDouble();
 
-        System.out.print("Enter Width of the Rectangle: ");
-        double rectWidth = scanner.nextDouble();
-
-        double rectArea = rectLength * rectWidth;
-        System.out.println("Area of the Rectangle is: " + rectArea + " cm²");
-
+        System.out.print("Quantity of the product: ");
+        int itemQuantity = scanner.nextInt();
+        double itemTotal = itemPrice * itemQuantity;
+        System.out.println("You have bought " + itemQuantity + " " + itemName + "/s");
+        System.out.println("Your total is: " + itemTotal + " Rs.");
         scanner.close();
-
     }
 }
