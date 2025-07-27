0d6683f8e9052efa2bb8f204cf0a9138f19ec160
Suhas Nag <suhas.nag@perficient.com>
2025-07-27 17:36:33 +0530

    Area of Rectangle Program

--- src/App.java
+++ src/App.java
@@ -1,54 +1,20 @@
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
-        /*
-         * Variable = a reusable container for a value.
-         * a variable behaves as if it was the value it contains
-         * 
-         * Primitive = Simple value stored directly in memory (Stack)
-         * Reference = memory address (stack) that points to the heap.
-         * 
-         * Primitive vs Reference
-         * --------- ---------
-         * int array
-         * double string
-         * char object
-         * boolean
-         */
-
-        String name = "Suhas Nag";
-        int gpa = 6;
-        char gender = 'M';
-        double pi = 3.14159;
-        boolean isAdmin = true;
-
-        System.out.println("My name is: " + name);
-        System.out.println("I have a gpa of " + gpa + " and I am an admin.");
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter your Name: ");
-        String userName = scanner.nextLine();
+        System.out.println("Welcome to finding Area of a rectangle.");
 
-        System.out.print("Enter your age: ");
-        int userAge = scanner.nextInt();
+        System.out.print("Enter Lenght of the Rectangle: ");
+        double rectLength = scanner.nextDouble();
 
-        System.out.print("Enter your GPA: ");
-        double userScore = scanner.nextDouble();
+        System.out.print("Enter Width of the Rectangle: ");
+        double rectWidth = scanner.nextDouble();
 
-        System.out.print("Are you an Admin (true/false): ");
-        boolean userIsAdmin = scanner.nextBoolean();
+        double rectArea = rectLength * rectWidth;
+        System.out.println("Area of the Rectangle is: " + rectArea + " cm²");
 
-        System.out.println("Welcome " + userName);
-        System.out.println("Your age is: " + userAge + "\nYou have scored: " + userScore);
-        if (userIsAdmin) {
-
-            System.out.println("User is an Admin");
-        } else {
-
-            System.out.println("User is an Admin");
-        }
         scanner.close();
+
     }
 }
