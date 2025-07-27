99f7b931536c7c01c5415c0b19eb15d423d0aa46
Suhas Nag <suhas.nag@perficient.com>
2025-07-27 19:01:49 +0530

    Simple Program to roll the dice

--- src/App.java
+++ src/App.java
@@ -1,19 +1,11 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the Item you would like to buy: ");
-        String itemName = scanner.nextLine();
+        Random random = new Random();
 
-        System.out.print("Enter the Price for each: ");
-        double itemPrice = scanner.nextDouble();
-
-        System.out.print("Quantity of the product: ");
-        int itemQuantity = scanner.nextInt();
-        double itemTotal = itemPrice * itemQuantity;
-        System.out.println("You have bought " + itemQuantity + " " + itemName + "/s");
-        System.out.println("Your total is: " + itemTotal + " Rs.");
-        scanner.close();
+        System.out.println("Rolling the Dice.");
+        int diceNum = random.nextInt(1, 7);
+        System.out.println("You got the number: " + diceNum);
     }
 }
