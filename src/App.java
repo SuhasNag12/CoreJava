afba1557489f515821b8916bade71faaad927ab5
Suhas Nag <suhas.nag@perficient.com>
2025-07-28 08:02:29 +0530

    Weight conversion

--- src/App.java
+++ src/App.java
@@ -1,28 +1,32 @@
+
+// This program converts KG to Pounds or Pounds to KG based on user input:
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        double principal;
-        double interestRate;
-        double time;
-        double number;
-        double output;
+        double kgWeight;
+        double lbsWeight;
+        double convertedWeight = 0.00;
 
-        System.out.print("Enter the Principal Amount: ");
-        principal = scanner.nextDouble();
+        System.out.println("Welcome to Weight Conversion Program: ");
+        System.out.println("1: Convert Lbs to KG");
+        System.out.println("2: Convert KG to Lbs");
+        System.out.print("Enter Your Choice (1 or 2): ");
+        int userInput = scanner.nextInt();
+        if (userInput == 1) {
+            System.out.print("Enter the Weight in Lbs: ");
+            lbsWeight = scanner.nextDouble();
+            convertedWeight = lbsWeight * 0.453592;
+        } else if (userInput == 2) {
+            System.out.print("Enter the Weight in Kgs: ");
+            kgWeight = scanner.nextDouble();
+            convertedWeight = kgWeight * 2.20462;
+        } else {
+            System.out.println("You have entered wrong Choice, Restart the Program");
 
-        System.out.print("Enter the Interest Rate (in %): ");
-        interestRate = scanner.nextDouble();
-
-        System.out.print("Enter the # of times compunded per year: ");
-        number = scanner.nextDouble();
-
-        System.out.print("Enter the # of years: ");
-        time = scanner.nextDouble();
-
-        output = principal * Math.pow(1 + (interestRate/100) / number, (number * time));
-
-        System.out.println("The amount after 1 years is: " + output);
+        }
+        System.out.printf("Converted Weight is: %.2f", convertedWeight);
+        scanner.close();
     }
 }
