1cc638cf7863f7d3919a804de715de119c5055f0
Suhas Nag <suhas.nag@perficient.com>
2025-07-30 16:51:24 +0530

    Program to illustrate Getters Setters

--- src/App.java
+++ src/App.java
@@ -1,27 +1,10 @@
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int userInput;
-        Animal animal;
-        System.out.print("Enter your choice for Dog or Cat (1 = Dog and 2 = Cat): ");
-        userInput = scanner.nextInt();
-        switch (userInput) {
-            case 1: {
-                animal = new Dog();
-                animal.speak();
-                break;
-            }
-            case 2: {
-                animal = new Cat();
-                animal.speak();
-                break;
-            }
-            default:
-                System.out.println("Error!! Invalid Input.");
-                break;
-        }
-        scanner.close();
+        Car car = new Car();
+        car.setColor("Blue");
+        car.setName("Tata Nexon");
+        car.setPrice(950000);
+        System.out.println(
+                "I bought " + car.getName() + " of color " + car.getColor() + " for a price of Rs" + car.getPrice());
     }
 }
---
+++ src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+
+    private String name;
+    private String color;
+    private int price;
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getName() {
+        return this.name;
+    }
+
+    void setColor(String color) {
+        this.color = color;
+    }
+
+    String getColor() {
+        return this.color;
+    }
+
+    void setPrice(int price) {
+        this.price = price;
+    }
+
+    int getPrice() {
+        return this.price;
+    }
+
+}
